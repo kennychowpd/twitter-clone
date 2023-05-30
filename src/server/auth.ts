9062dd200015246572a36f1c2763b565c3c67d77
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-30 16:08:03 +0800

    auth.ts bug fix

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -48,9 +48,10 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+
     /**
      * ...add more providers here.
      *
